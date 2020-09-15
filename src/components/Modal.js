01f9d5fb34d526b193ce0bc0f3b2fe4ffd8cf177
zohresalimi <zohre.1987@gmail.com>
2020-09-15 15:12:02 +0200

    fix arrow btns in carousel slider

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useEffect, useContext, useRef } from 'react';
 import ArrowRightAlt from '@material-ui/icons/ArrowRightAlt';
 import ImagesContext from '../context/ImagesContext'
 
@@ -6,13 +6,13 @@ import './modal.css'
 
 const SelectedImage = () =>{
     const { images, selectedImage, setSelectedImage } = useContext(ImagesContext)
+    console.log(images)
     const img = images.find((image) => image.id === selectedImage)
     console.log(img)
     return(
         <div className="carousel-inner">
             <div className="item">
                 <img src={img.urls.regular}/>
-
             </div>
         </div>
     )
@@ -22,7 +22,13 @@ const SelectedImage = () =>{
 
 function Modal(props) {
     const { images, selectedImage, setSelectedImage } = useContext(ImagesContext)
-    const [transform, setTransform] = useState(0)
+    const currentElement = useRef(null)
+    const swiperWrapper = useRef(null)
+    const swiperContainer = useRef(null)
+
+    useEffect(() => {
+        goToElement()
+    }, [selectedImage])
 
     const closeModal= () => {
         props.setModalShow(false)
@@ -40,7 +46,6 @@ function Modal(props) {
         }
         const nextImage = images[currImageIndex+1].id;
         setSelectedImage(nextImage)
-        console.log(nextImage)
     }
 
     const handelPrevButton = () =>{
@@ -52,34 +57,11 @@ function Modal(props) {
         const nextImage = images[currImageIndex-1].id;
         setSelectedImage(nextImage)
     }
-
-
-    const transformSliderItems = (e) => {
-        const newTransform = `translate(${transform + 150}px, 0px)`;
-        setTransform(newTransform)
-      }
-
-
-
-      const elementInViewport = (el) => {
-        var top = el.offsetTop;
-        var left = el.offsetLeft;
-        var width = el.offsetWidth;
-        var height = el.offsetHeight;
       
-        while(el.offsetParent) {
-          el = el.offsetParent;
-          top += el.offsetTop;
-          left += el.offsetLeft;
-        }
-      
-        return (
-          top >= window.pageYOffset &&
-          left >= window.pageXOffset &&
-          (top + height) <= (window.pageYOffset + window.innerHeight) &&
-          (left + width) <= (window.pageXOffset + window.innerWidth)
-        );
-      }
+    function goToElement() {
+        const parentElement = swiperContainer.current
+        parentElement.scrollTo({top: 0, left: currentElement.current.offsetLeft, behavior: 'smooth'})
+    }
 
     return (
 
@@ -89,25 +71,17 @@ function Modal(props) {
                 <label className="modal__close" onClick={closeModal}></label>
                 <SelectedImage ></SelectedImage>
                 <div className="swiper-thumb">
-                    <div className="swiper-container" >
-                        <div className="swiper-wrapper">
+                    <div className="swiper-container" ref={swiperContainer}>
+                        <div className="swiper-wrapper" ref={swiperWrapper}>
                             {images.map((img)=> { 
                                 return (
-                                    <div className="slide" key={img.id} onClick={() => selectImage(img.id)}>
+                                    <div className={`slide ${selectedImage === img.id ?"img-filter":""}`} key={img.id} onClick={() => selectImage(img.id)} ref={selectedImage === img.id ?currentElement: null}>
                                         <div className="image-wrapper">
                                             <img src={img.urls.thumb} className="image-thumb"/>
                                             <span className="overlay">
                                                 <div className="authorAvatar">
                                                     <img src={img.user.profile_image}/>
                                                 </div>
-                                                {/* <div className="titleAndAuthor">
-                                                    <h3 className="title">
-                                                        <a href="">{img.description}</a>
-                                                    </h3>
-                                                    <p className="authorName">
-                                                        <a href="">{img.user.name}</a>
-                                                    </p>
-                                                </div> */}
                                             </span>
                                         </div>
                                     </div>
@@ -115,7 +89,7 @@ function Modal(props) {
                             })}
                         </div>
                     </div>
-                    <div className="swiper-next-button" onClick={handelNextButton} style={{transform: transformSliderItems}}>
+                    <div className="swiper-next-button" onClick={handelNextButton}>
                             <em className="material-icons"><ArrowRightAlt></ArrowRightAlt></em>
                     </div>
                     <div className="swiper-prev-button" onClick={handelPrevButton}>
